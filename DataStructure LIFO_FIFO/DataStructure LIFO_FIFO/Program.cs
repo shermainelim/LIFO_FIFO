39e8257ab42892f9fadf9e6a14ba697807185936
shermaine <shermaine.lim.si.hui@gmail.com>
2021-04-26 20:55:40 +0800

    Commit 1

---
+++ DataStructure LIFO_FIFO/DataStructure LIFO_FIFO/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure_LIFO_FIFO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+
+            //fill the queue
+            foreach(int number in new int[8] { 15, 47, 38, 89, 2, 61, 53, 24 })
+            {
+                numbers.Enqueue(number);
+                Console.WriteLine("{0} has joined the queue", number);
+            }
+
+            //interate through the queue
+            Console.WriteLine("\nThe queue contains the following items:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            //Dequeue remove it
+            Console.WriteLine("Removing items:");
+            while (numbers.Count > 0)
+            {
+                int number = numbers.Dequeue();
+                Console.WriteLine("{0} has left the queue", number);
+            }
+
+            //stack
+
+            Stack<int> number1 = new Stack<int>();
+
+            //fill the sstack
+            Console.WriteLine("Pushing items to the stack:");
+            foreach(int number12 in new int[8] { 15, 47, 38, 89, 2, 61, 53, 24 })
+            {
+                number1.Push(number12);
+                Console.WriteLine("{0} has been pushed on the stack", number12);
+            }
+
+            //iterate through the stack
+            Console.WriteLine("\n The stack now contain:");
+            foreach(int number12 in number1)
+            {
+                Console.WriteLine(number12);
+            }
+
+            //empty the stack
+            while(number1.Count>0)
+            {
+                int number12 = number1.Pop();
+                Console.WriteLine("{0} has been popped", number12);
+            }
+        }
+    }
+}
